af40e614b5012145e8fad30a01f9e31520ad5ec1
rigoxls <rigoxls@gmail.com>
2015-04-21 16:13:10 -0500

    improving input-file directive, show image before to load

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,15 +7,30 @@
             }
         })
 
-        .directive('file', function () {
+        .directive('inputFile', function () {
             return {
+                restrict: 'E',
+                templateUrl: '../partials/inputFile.html',
                 scope: {
-                    file: '='
+                    file: '=',
+                    image: '='
                 },
-                link: function (scope, el, attrs) {
-                    el.bind('change', function (event) {
+                link: function (scope, els, attrs) {
+                    //attrs => from <input-file> , els => from inputFile.html, scope => isolate scope
+                    var els = els.children();
+                    var inputFile = angular.element(els[0]);
+
+                    var reader = new FileReader();
+                    reader.onload = function(e){
+                        scope.image = e.target.result;
+                        scope.$apply();
+                    }
+
+                    inputFile.bind('change', function (event) {
                         var file = event.target.files[0];
                         scope.file = file ? file : undefined;
+
+                        reader.readAsDataURL(file);
                         scope.$apply();
                     });
                 }
